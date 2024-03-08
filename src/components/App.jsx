e6ce6fe1ba7b6bcd0344004364ef9acbc499d9d5
Victor Diez <victordiez1@gmail.com>
2024-03-08 11:17:03 +0100

    Mise en place des couleurs, des fonts et des differents dossiers

---
+++ src/components/App.jsx
@@ -0,0 +1,21 @@
+import '../styles/reset.scss'
+import Error from '../page/Error'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Portfolio from '../page/Portfolio'
+import '../styles/app.scss'
+
+function App() {
+
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Portfolio/>}/>
+          <Route path='*' element={<Error/>}/>
+        </Routes>
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default App
---
+++ src/page/Portfolio.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from '../components/header'
+import Footer from'../components/footer'
+import "../styles/portfolio.scss"
+
+export default function Portfolio() {
+
+    return (
+        <>
+            <Header/>
+            <Footer/>
+        </>
+    )
+}
