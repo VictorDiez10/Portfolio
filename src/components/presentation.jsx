8e973b2b108c14dc1426081004a069458e23d66a
Victor Diez <victordiez1@gmail.com>
2024-05-02 16:29:18 +0200

    Balise picture enlevé remplacé par la balise img, ajout des mentions, message stylisé lors de l'envoi d'un message

--- src/components/presentation.jsx
+++ src/components/presentation.jsx
@@ -1,14 +1,24 @@
-import '../styles/presentation.scss'
-import original from '../assets/images/grand.jpg'
+import '../styles/presentation.scss';
+import grand from '../assets/images/grand.jpg';
+import moyen from '../assets/images/moyen.jpg';
+import petit from '../assets/images/petit.jpg';
 
 export default function presentation() {
   return (
     <div className='main-content'>
-        <picture>
-          <source media="" srcset={original}/>
-          <img src={original} alt="#" />
-        </picture>
+        {/* <picture> */}
+          {/* <source srcSet={` ${petit} 640w, ${moyen} 1280w, ${grand} 1920w`}
+          sizes="(max-width: 640px) 640px, (max-width: 1280px) 1280px, 1920px"
+          /> */}
+          
+          {/* <source media="(max-width: 1280px)" srcSet={moyen}/>
+          <source media="(max-width: 640px)" srcSet={petit}/> */}
+          <img src={grand} alt="#" srcSet={` ${petit} 640w, ${moyen} 1280w, ${grand} 1920w`}/>
+          
+        {/* </picture> */}
+        <div className="titre-p">
           <h1>Bonjour je suis Victor developeur Front-end</h1>
+          </div>
     </div>
   )
 }
--- src/page/MentionsLegales.jsx
+++ src/page/MentionsLegales.jsx
@@ -34,6 +34,24 @@ Victor Diez, de nationalité Française (France), né(e) le 12/04/2003,</p>
 Par email : victordiez1@gmail.com
 </p>
 
+
+<h2>Credits</h2>
+
+{/* <div className="credits"> */}
+
+<a href="https://www.flaticon.com/fr/icone-gratuite/developpement-de-logiciels_6656137" target='_blank'>Développement de logiciels icônes créées par wanicon - Flaticon</a>
+<a href="https://www.flaticon.com/fr/icone-gratuite/serveur_4492031"  target='_blank'>Serveur icônes créées par wanicon - Flaticon</a>
+<a href="https://www.flaticon.com/fr/icone-gratuite/crayon_473695?related_id=473613&origin=search" target='_blank'>Crayon de logiciels icônes créées par wanicon - Flaticon</a>
+<a href="https://svgmix.com/item/XJQENg/react" target='_blank'>Par Meta Platforms</a>
+<a href="https://commons.wikimedia.org/w/index.php?curid=26936716" target="_blank" rel="noopener noreferrer">Par Ryan Dahl</a>
+<a href="https://www.pngwing.com/en/free-png-iuofv" target="_blank" rel="noopener noreferrer">Par Pngwing</a>
+<a href="https://commons.wikimedia.org/w/index.php?curid=20806506" target="_blank" rel="noopener noreferrer">Par Chris Williams — Travail personnel avec: GitHub.com</a>
+<a href="https://icon-icons.com/fr/icone/figma-logo/171159" target="_blank" rel="noopener noreferrer">Par Vector Lego</a>
+<a href="https://commons.wikimedia.org/w/index.php?curid=12736763" target="_blank" rel="noopener noreferrer">Par W3C, CC BY 3.0</a>
+<a href="https://commons.wikimedia.org/w/index.php?curid=5494388" target="_blank" rel="noopener noreferrer">Par Tuomas Kuosmanen — The GIMP package</a>
+<a href="https://commons.wikimedia.org/w/index.php?curid=49121103" target="_blank" rel="noopener noreferrer">Par Rudloff — CSS3, CC BY 3.0</a>
+{/* </div> */}
+
 <h2>Generation des mentions legales par Legalstart.</h2>
 
 
