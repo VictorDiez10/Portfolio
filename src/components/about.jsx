59f5c345447a4e3bbe021a43e211cfeb239aae27
Victor Diez <victordiez1@gmail.com>
2024-05-03 13:10:51 +0200

    Message dans a propos

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -5,7 +5,8 @@ export default function Propos() {
         <section id='about'>
             <h2>A PROPOS</h2>
             <div className="about">
-                <p>Toujours attiré par l'informatique, j'ai décidé de suivre une fomation pour de venir développeur web</p>
+                <p>Passionné par l'informatique depuis mon plus jeune âge, j'ai toujours été fasciné par la manière dont les technologies numériques transforment notre monde. Cette fascination m'a poussé à approfondir mes connaissances et compétences dans ce domaine. </p>
+                <p>Finalement, cette passion m'a conduit à choisir une carrière dans le domaine de l'informatique. Aujourd'hui, travailler dans l'informatique me permet de concilier ma passion avec ma vie professionnelle, et chaque jour, je suis enthousiasmé par les défis et les possibilités qu'offre ce domaine en constante évolution.</p>
             </div>
         </section>
     )
